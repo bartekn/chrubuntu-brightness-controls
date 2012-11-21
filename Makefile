2b9507098f4c3a619614ed2abf5106110c808d97
Willi Ballenthin <willi.ballenthin@gmail.com>
2012-11-21 22:20:15 +0000

    increase debugging info, consolidate code

--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@
 all: up down
 
 
-up: brightness-up.c
-	clang -g -Wall -o brightness-up brightness-up.c
+up: brightness.c
+	clang -g -Wall -o brightness-up -D BRIGHTNESS_UP brightness.c
 
 
-down: brightness-down.c
-	clang -g -Wall -o brightness-down brightness-down.c
+down: brightness.c
+	clang -g -Wall -o brightness-down -D BRIGHTNESS_DOWN brightness.c
 
 
 
--- brightness.c
+++ brightness.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 static char brightness_path[] = "/sys/devices/platform/s3c24xx-pwm.0/pwm-backlight.0/backlight/pwm-backlight.0/brightness";
 
-#define INCREMENT 10
+#ifdef BRIGHTNESS_UP
+  #define INCREMENT 25
+#endif
+
+#ifdef BRIGHTNESS_DOWN
+  #define INCREMENT -25
+#endif
 
 int main(int argc, char **argv) {
   char brightness_string[4];
@@ -13,13 +20,13 @@ int main(int argc, char **argv) {
   int ret;
   FILE *fp = fopen(brightness_path, "r+");
   if (fp == NULL) {
-    printf("FAILED to open file.\n");
+    printf("FAILED to open file (%d, %d).\n", 0, errno);
     return -1;
   }
   
   read = fread(brightness_string, 1, 4, fp);
-  if (read != 4) {
-    printf("FAILED to read file.\n");
+  if (read > 4) {
+    printf("FAILED to read file (%d, %d).\n", read, errno);
     fclose(fp);
     return -2;
   }
@@ -31,14 +38,18 @@ int main(int argc, char **argv) {
     return -3;
   }
 
-  brightness -= INCREMENT;
+  brightness += INCREMENT;
+  if (brightness > 255) {
+    brightness = 255;
+  }
+
   if (brightness < 0) {
     brightness = 0;
   }
 
   ret = snprintf(brightness_string, 4, "%d", brightness);
   if (ret < 0) {
-    printf("FAILED to convert brightness to string.\n");
+    printf("FAILED to convert brightness to string (%d, %d).\n", ret, errno);
     fclose(fp);
     return -4;
   }
@@ -46,7 +57,7 @@ int main(int argc, char **argv) {
   rewind(fp);
   read = fwrite(brightness_string, 1, 4, fp);
   if (read != strlen(brightness_string) + 1) {
-    printf("FAILED to write file.\n");
+    printf("FAILED to write file (%d, %d).\n", read, errno);
     fclose(fp);
     return -5;
   }
